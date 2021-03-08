03afa629496c5084e5c8b1703b034f1fc7a3aa66
Forrest Bicker <bickerforrest@gmail.com>
2021-03-08 18:15:54 -0500

    restructure calcTheta function

--- ScratchWork/util.py
+++ ScratchWork/util.py
@@ -1,27 +1,28 @@
 import math
-from tf_pose import common
+from tf_pose.common import CocoPart, CocoPairsRender
 import numpy as np
 
 def calcTheta(humans):
-	for human in humans:
-		limbs = {}
-		centers = {}
-		for i in range(common.CocoPart.Background.value):
-			if i not in human.body_parts.keys():
-				continue
+    for human in humans:
+        limbs = {}
+        centers = {}
+        for i in range(CocoPart.Background.value):
+            if i not in human.body_parts.keys():
+                continue
 
-			body_part = human.body_parts[i]
-			center = (body_part.x, body_part.y)
-			centers[common.CocoPart(i)] = center
+            body_part = human.body_parts[i]
+            center = (body_part.x, body_part.y)
+            centers[CocoPart(i)] = center
 
-		for pair_order, pair in enumerate(common.CocoPairsRender): # not exactly sure why this is looping, isnt it just going to end up overwriting the dict? - forrest
-			if pair[0] not in human.body_parts.keys() or pair[1] not in human.body_parts.keys():
-				continue
-			try:
-				limbs["right"] = np.subtract(centers[common.CocoPart.RHip], centers[common.CocoPart.RKnee])
-				limbs["left"] = np.subtract(centers[common.CocoPart.LHip], centers[common.CocoPart.LKnee])
-			except KeyError:
-				return 0 # if cant detect r or l hip
+        for pair_order, pair in enumerate(CocoPairsRender):  # not exactly sure why this is looping, isnt it just going to end up overwriting the dict? - forrest
+            if pair[0] not in human.body_parts.keys() or pair[1] not in human.body_parts.keys():
+                continue
+            try:
+                limbs["right"] = np.subtract(centers[CocoPart.RHip], centers[CocoPart.RKnee])
+                limbs["left"] = np.subtract(centers[CocoPart.LHip], centers[CocoPart.LKnee])
+            except KeyError:
+                return 0  # if cant detect r or l hip
 
-		return limbs.get("right").dot(limbs.get("left")) / (np.linalg.norm(limbs.get("right")) * (np.linalg.norm(limbs.get("left"))))
-	return 0 # no humans in frame
+        return limbs.get("right").dot(limbs.get("left")) / (np.linalg.norm(limbs.get("right")) * (np.linalg.norm(limbs.get("left"))))
+    return 0  # no humans in frame
+
