87b6e59c3408b67505b02e00baeb1ccc468ad7cb
CaowardlyLion <kaoerik@gmail.com>
2021-03-15 17:17:26 -0400

    combined forrest's code with mine to see which one is better; also cleaned up unecessary loop

--- ScratchWork/util.py
+++ ScratchWork/util.py
@@ -2,8 +2,10 @@ import math
 from tf_pose import common
 import numpy as np
 from enum import Enum
+from tf_pose.common import CocoPart
 
-class Angle(Enum): # the index for the angle with the joint centnered at name
+
+class Angle(Enum):  # the index for the angle with the joint centnered at name
     LElbow = 0
     LShoulder = 1
     LHip = 2
@@ -13,29 +15,29 @@ class Angle(Enum): # the index for the angle with the joint centnered at name
     RHip = 6
     RKnee = 7
 
+
 def calcTheta(humans):
     for human in humans:
         limbs = {}
         centers = {}
-        for i in range(CocoPart.Background.value):
+        for i in range(common.CocoPart.Background.value):
             if i not in human.body_parts.keys():
                 continue
 
             body_part = human.body_parts[i]
             center = (body_part.x, body_part.y)
-            centers[CocoPart(i)] = center
+            centers[common.CocoPart(i)] = center
 
-        for pair_order, pair in enumerate(CocoPairsRender):  # not exactly sure why this is looping, isnt it just going to end up overwriting the dict? - forrest
-            if pair[0] not in human.body_parts.keys() or pair[1] not in human.body_parts.keys():
-                continue
-            try:
-                limbs["right"] = np.subtract(centers[CocoPart.RHip], centers[CocoPart.RKnee])
-                limbs["left"] = np.subtract(centers[CocoPart.LHip], centers[CocoPart.LKnee])
-            except KeyError:
-                return 0  # if cant detect r or l hip
+        try:
+            limbs["right"] = np.subtract(centers[common.CocoPart.RHip], centers[common.CocoPart.RKnee])
+            limbs["left"] = np.subtract(centers[common.CocoPart.LHip], centers[common.CocoPart.LKnee])
+        except KeyError:
+            return 0  # if cant detect r or l hip
 
-        return limbs.get("right").dot(limbs.get("left")) / (np.linalg.norm(limbs.get("right")) * (np.linalg.norm(limbs.get("left"))))
+        return limbs.get("right").dot(limbs.get("left")) / (
+                    np.linalg.norm(limbs.get("right")) * (np.linalg.norm(limbs.get("left"))))
     return 0  # no humans in frame
+
 
 def cos(bp, point1, rootPoint, point2):
     try:
@@ -70,28 +72,33 @@ def calcThetas(human):
 
     return angle_vector
 
-def calcBodyThetas(humans):
-	for human in humans:
-		limbs = {}
-		centers = {}
-		bodyAngles = [(0,12), (1,12), (0,2), (1, 4), (2, 3), (4, 5), (7, 10), (7, 8), (10, 11)]
-		calcAngles = []
-		for i in range(common.CocoPart.Background.value):
-			if i not in human.body_parts.keys():
-				continue
 
-			body_part = human.body_parts[i]
-			center = (body_part.x, body_part.y)
-			centers[common.CocoPart(i)] = center
-		limblabels = ["rightSN, leftSN, rightBicep, rightArm, leftBicep, leftArm, rightTorso, rightThigh, rightShin, leftTorso, leftThigh, leftShin, face"]
-		for pair_order, pair in enumerate(common.CocoPairsRender[0:13]): # not exactly sure why this is looping, isnt it just going to end up overwriting the dict? - forrest
-			if pair[0] not in human.body_parts.keys() or pair[1] not in human.body_parts.keys():
-				continue
-			try:
-				limbs[limblabels[pair_order]] = np.subtract(centers[pair[0]], centers[pair[1]])
-			except KeyError:
-				return 0 # if cant detect r or l hip
-		for bodyangle in bodyAngles:
-			calcAngles.append(limbs.get(limblabels[bodyangle[0]]).dot(limbs.get(bodyangle[1])) / (np.linalg.norm(limbs.get(limblabels[bodyangle[0]])) * (np.linalg.norm(limbs.get(limblabels[bodyangle[1]])))))
-		return calcAngles
-	return 0 # no humans in frame
+def calcBodyThetas(humans):
+    for human in humans:
+        limbs = {}
+        centers = {}
+        bodyAngles = [(0, 12), (1, 12), (0, 2), (1, 4), (2, 3), (4, 5), (7, 10), (7, 8), (10, 11)]
+        calcAngles = []
+        for i in range(common.CocoPart.Background.value):
+            if i not in human.body_parts.keys():
+                continue
+
+            body_part = human.body_parts[i]
+            center = (body_part.x, body_part.y)
+            centers[common.CocoPart(i)] = center
+        limblabels = ["rightSN", "leftSN", "rightBicep", "rightArm", "leftBicep", "leftArm", "rightTorso", "rightThigh",
+                      "rightShin", "leftTorso", "leftThigh", "leftShin", "face"]
+        for pair_order, pair in enumerate(common.CocoPairsRender[0:13]):  # not exactly sure why this is looping, isnt
+            # it just going to end up overwriting the dict? - forrest
+            if pair[0] not in human.body_parts.keys() or pair[1] not in human.body_parts.keys():
+                continue
+            try:
+                limbs[limblabels[pair_order]] = np.subtract(centers[pair[0]], centers[pair[1]])
+            except KeyError:
+                return 0  # if cant detect r or l hip
+        for bodyangle in bodyAngles:
+            calcAngles.append(limbs.get(limblabels[bodyangle[0]]).dot(limbs.get(bodyangle[1])) / (
+                        np.linalg.norm(limbs.get(limblabels[bodyangle[0]])) * (
+                    np.linalg.norm(limbs.get(limblabels[bodyangle[1]])))))
+        return calcAngles
+    return 0  # no humans in frame
