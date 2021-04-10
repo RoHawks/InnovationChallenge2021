9febc4577fc96c57929acf955de4843abe568a51
Forrest Bicker <bickerforrest@gmail.com>
2021-04-10 10:37:40 -0400

    host html, js, css components on webserver

--- ScratchWork/apitest.py
+++ ScratchWork/apitest.py
@@ -1,11 +1,37 @@
 import flask
-from flask import request, jsonify
+from flask import request, jsonify, Response
 import json
 import requests
 
 app = flask.Flask(__name__)
 
 scores = {"test": "something"}
+
+@app.route("/background.js")
+def js():
+    with open('docs/background.js', 'r') as f:
+        js = f.read()
+        return js
+
+@app.route("/style.css")
+def css():
+    with open('docs/style.css', 'r') as f:
+        css = f.read()
+        return Response(css, mimetype="text/css")
+
+@app.route("/assets/logo.png")
+def logo():
+    with open('docs/assets/logo.png', 'rb') as f:
+        logo = f.read()
+        return Response(logo, mimetype="image/png")
+
+# Annotation that allows the function to be hit at the specific URL.
+@app.route("/")
+# Generic Python functino that returns "Hello world!"
+def index():
+    with open('docs/index.html', 'r') as f:
+        html = f.read()
+        return html
 
 @app.route('/show', methods=['GET'])
 def show():
